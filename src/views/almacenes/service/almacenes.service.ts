00f762cb12319ed725b2b35d207cebd3252dd4b4
Boyita Boy <anabelensanchezboy@gmail.com>
2026-03-20 00:03:24 -0500

    cambio en manejo del path en almacenes

--- src/views/almacenes/service/almacenes.service.ts
+++ src/views/almacenes/service/almacenes.service.ts
@@ -12,87 +12,57 @@ import type {
   RES_ResponsableAlmacen,
 } from "./almacenes.responses";
 
-export class AlmacenesService {
-  private static PATH = "/almacenes";
+const PATH = "/almacenes";
 
+export const AlmacenesService = {
   // ALMACENES
-
-  public static get_almacenes = async (): Promise<
-    IRespuesta<RES_Almacen[]>
-  > => {
-    const { data } = await api.get(`${this.PATH}`);
+  get_almacenes: async (): Promise<IRespuesta<RES_Almacen[]>> => {
+    const { data } = await api.get(PATH);
     return data;
-  };
+  },
 
-  public static crear_almacen = async (
-    dto: DTO_CrearAlmacen,
-  ): Promise<IRespuesta<RES_Almacen>> => {
-    const { data } = await api.post(`${this.PATH}`, dto);
+  crear_almacen: async (dto: DTO_CrearAlmacen): Promise<IRespuesta<RES_Almacen>> => {
+    const { data } = await api.post(PATH, dto);
     return data;
-  };
+  },
 
   // RESPONSABLES
-
-  public static get_historial_responsables = async (
-    id_almacen: number,
-  ): Promise<IRespuesta<RES_ResponsableAlmacen[]>> => {
-    const { data } = await api.get(`${this.PATH}/responsables/${id_almacen}`);
+  get_historial_responsables: async (id_almacen: number): Promise<IRespuesta<RES_ResponsableAlmacen[]>> => {
+    const { data } = await api.get(`${PATH}/responsables/${id_almacen}`);
     return data;
-  };
+  },
 
-  public static nuevo_responsable = async (
-    dto: DTO_NuevoResponsable,
-  ): Promise<IRespuesta<RES_ResponsableAlmacen>> => {
-    const { data } = await api.post(`${this.PATH}/responsables`, dto);
+  nuevo_responsable: async (dto: DTO_NuevoResponsable): Promise<IRespuesta<RES_ResponsableAlmacen>> => {
+    const { data } = await api.post(`${PATH}/responsables`, dto);
     return data;
-  };
+  },
 
-  public static get_empleados = async (
-    id_almacen: number,
-  ): Promise<IRespuesta<RES_EmpleadoDisponible[]>> => {
-    const { data } = await api.post(
-      `${this.PATH}/responsables/empleados/${id_almacen}`,
-    );
+  get_empleados: async (id_almacen: number): Promise<IRespuesta<RES_EmpleadoDisponible[]>> => {
+    const { data } = await api.post(`${PATH}/responsables/empleados/${id_almacen}`);
     return data;
-  };
+  },
 
   // ABASTECIMIENTO DE MINAS
-
-  public static get_minas_abastecidas = async (
-    id_almacen: number,
-  ): Promise<IRespuesta<RES_MinaAbastecida[]>> => {
-    const { data } = await api.get(
-      `${this.PATH}/abastecimiento-minas/${id_almacen}`,
-    );
+  get_minas_abastecidas: async (id_almacen: number): Promise<IRespuesta<RES_MinaAbastecida[]>> => {
+    const { data } = await api.get(`${PATH}/abastecimiento-minas/${id_almacen}`);
     return data;
-  };
+  },
 
-  public static nueva_mina_por_abastecer = async (
-    id_almacen: number,
-    id_mina: number,
-  ): Promise<IRespuesta<RES_MinaAbastecida>> => {
-    const { data } = await api.post(`${this.PATH}/abastecimiento-minas`, {
-      id_almacen: id_almacen,
-      id_mina: id_mina,
+  nueva_mina_por_abastecer: async (id_almacen: number, id_mina: number): Promise<IRespuesta<RES_MinaAbastecida>> => {
+    const { data } = await api.post(`${PATH}/abastecimiento-minas`, {
+      id_almacen,
+      id_mina,
     });
     return data;
-  };
+  },
 
-  public static eliminar_abastecimiento_mina = async (
-    id_almacen_mina: number,
-  ): Promise<IRespuesta<null>> => {
-    const { data } = await api.delete(
-      `${this.PATH}/abastecimiento-minas/${id_almacen_mina}`,
-    );
+  eliminar_abastecimiento_mina: async (id_almacen_mina: number): Promise<IRespuesta<null>> => {
+    const { data } = await api.delete(`${PATH}/abastecimiento-minas/${id_almacen_mina}`);
     return data;
-  };
+  },
 
-  public static get_minas = async (
-    id_almacen: number,
-  ): Promise<IRespuesta<RES_MinaDisponible[]>> => {
-    const { data } = await api.get(
-      `${this.PATH}/abastecimiento-minas/minas/${id_almacen}`,
-    );
+  get_minas: async (id_almacen: number): Promise<IRespuesta<RES_MinaDisponible[]>> => {
+    const { data } = await api.get(`${PATH}/abastecimiento-minas/minas/${id_almacen}`);
     return data;
-  };
-}
+  },
+};
