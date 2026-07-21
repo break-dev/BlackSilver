b4d7f24e64c3679081742f6254153f454ceccb2f
Break <franklinbacacampos@gmail.com>
2026-07-21 16:32:41 -0500

    aumentar limite de caracteres en el prefijo de productos

--- src/modules/productos/presentation/registro-producto.tsx
+++ src/modules/productos/presentation/registro-producto.tsx
@@ -273,7 +273,7 @@ export const RegistroProducto = ({
                 <TextInput
                   label={<LabelForm text="Prefijo (Activo Fijo)" />}
                   placeholder="Ej: SCOO"
-                  maxLength={4}
+                  maxLength={24}
                   value={form.prefijo || ""}
                   onChange={(e) =>
                     setField("prefijo", e.currentTarget.value.toUpperCase())
